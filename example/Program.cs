97c36e5a00f3be226470c9f5fcc76cd7e53217a3
Anna <anna.93.08@mail.ru>
2022-09-05 22:34:30 +0300

    Решение задачи

--- example/Program.cs
+++ example/Program.cs
@@ -1,6 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
-
-// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
 //длина которых меньше, либо равна 3 символам.
 
 string[] array = {"Hello", "2", "world", ":-)"};
+string[] NewArray = new string[array.Length];
+
+
+void NewArrayIf(string[] array, string[] NewArray)
+{
+    int size = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        NewArray[size] = array[i];
+        size++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+NewArrayIf(array, NewArray);
+PrintArray(NewArray);
